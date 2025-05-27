691907a3c803513ecbe8ec4628f4dc3766764cfa
Trisha Goodwin <trishabromero@gmail.com>
2025-05-26 20:04:19 -0500

    everything done except integer issues and something weird with update

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -16,7 +16,12 @@ export async function createEmployee( {name, birthday, salary} ) {
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const sql = `
+  SELECT *
+  FROM employees;
+  `;
+  const { rows: employees } = await db.query(sql);
+  return employees;
 }
 
 /**
@@ -24,15 +29,28 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const sql = ` 
+   SELECT * FROM employees WHERE id = $1;
+  `
+
+  const {rows: employee} = await db.query(sql, [id])
+  return employee[0]
 }
 
 /**
  * @returns the updated employee with the given id
  * @returns undefined if employee with the given id does not exist
  */
-export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+export async function updateEmployee({id, name, birthday, salary}) {
+  const sql =`
+  UPDATE employees 
+  SET name = $1 , birthday = $2, salary = $3
+  WHERE id = $4
+  RETURNING * 
+  `
+
+const {rows: employee} = await db.query(sql, [name, birthday, salary, id])
+return employee[0]
 }
 
 /**
@@ -40,5 +58,11 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+  const sql = `
+    DELETE FROM employees WHERE id = $1 RETURNING *;
+  `
+
+  const {rows: employee} = await db.query(sql, [id])
+  
+  return employee[0]
 }
